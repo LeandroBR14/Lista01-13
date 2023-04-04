172ce49a6c6df8c73cc521a9838cf4dbe89ceab0
Leandro Scalabrini <scalabrinileandro12@gmail.com>
2023-04-04 07:27:37 -0300

    Atualização 1

--- src/App.java
+++ src/App.java
@@ -1,21 +1,24 @@
 import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
-        double altura;
-
         Scanner teclado = new Scanner(System.in);
 
+        System.out.println("Digite (M) caso seja mulher e (H) caso seja homem.");
+        String sexo = teclado.next();
         System.out.println("Digite sua altura: ");
-        altura = teclado.nextDouble();
+        double altura = teclado.nextDouble();
 
         double pesoidealHOMEM = (72.7*altura) - 58;
         double pesoidealMULHER = (62.1*altura) - 44.7;
 
-        System.out.print("Seu peso ideal caso seja homem é de: " +pesoidealHOMEM);
-        System.out.println(" kg");
-        System.out.print("Seu peso ideal caso seja mulher é de: " +pesoidealMULHER);
-        System.out.println(" kg");
-
+        if (sexo.equals("H")) {
+            System.out.printf("Seu peso ideal é de: %.2f kg", pesoidealHOMEM);
+        } else if (sexo.equals("M")) {
+            System.out.printf("Seu peso ideal é de: %.2f kg", pesoidealMULHER);
+        } else {
+            System.out.println("Sexo inválido, tente novamente.");
+        }
+        
         teclado.close();
     }
 }
